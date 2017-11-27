fc19f0f1abdb6d8d374121582b4a2ead1e8c27a3
student <mwoolery@ch112017m.nwmissouri.edu>
2017-11-27 14:44:41 -0600

    asdf

--- LookMaItDraws/SquiggleView.swift
+++ LookMaItDraws/SquiggleView.swift
@@ -12,25 +12,30 @@ class SquiggleView: UIView {
     
     
     var dots:[CGPoint] = []
-    var squiggle:[[CGPoint]] = [[]]
-    var currentColor:UIColor = UIColor.black
+    var squiggle:[[CGPoint]] = []
+    var colors:[UIColor] = [UIColor.black, UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    var colorIndex:Int = 0
+    var currentColor:[Int] = []
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         dots.append(touches.first!.location(in: self))
-        
+        currentColor.append(colorIndex)
         setNeedsDisplay()
         
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         dots.append(touches.first!.location(in: self))
-        
+        currentColor.append(colorIndex)
         setNeedsDisplay()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         dots.append(touches.first!.location(in: self))
+        squiggle.append(dots)
+        currentColor.append(colorIndex)
         
+        dots = []
         setNeedsDisplay()
     }
     
@@ -38,38 +43,53 @@ class SquiggleView: UIView {
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
-        if dots.count == 0 {
+        if squiggle.count == 0 {
+            return
+        }
+        if squiggle[0].count == 0{
             return
         }
         let bezier = UIBezierPath()
-        bezier.move(to:dots[0])
-        for dot in dots {
-            bezier.addLine(to: dot)
+        
+        for line in 0...squiggle.count - 1 {
             
+            bezier.move(to:squiggle[line][0])
+            for dot in 0...squiggle[line].count - 1{
+                bezier.addLine(to: squiggle[line][dot])
+                
+            }
+            let colorSetting = currentColor[line]
+            print(colorSetting)
+            colors[colorSetting].setStroke()
+            bezier.stroke()
         }
+        
+        //        for dot in dots {
+//            bezier.addLine(to: dot)
+//
+//        }
  
-        currentColor.setStroke()
-        bezier.stroke()
+        
         
     }
     
     @IBAction func purpleBTN(_ sender: Any) {
-        currentColor = UIColor.purple
+        colorIndex = 6
     }
     @IBAction func blueBTN(_ sender: Any) {
-       currentColor = UIColor.blue
+       colorIndex = 5
     }
     @IBAction func greenBTN(_ sender: Any) {
-        currentColor = UIColor.green
+        colorIndex = 4
     }
     @IBAction func yellowBTN(_ sender: Any) {
-        currentColor = UIColor.yellow
+        colorIndex = 3
     }
     @IBAction func orangeBTN(_ sender: Any) {
-        currentColor = UIColor.orange
+        colorIndex = 2
     }
     @IBAction func redBTN(_ sender: Any) {
-        currentColor = UIColor.red
+        colorIndex = 1
     }
     
 }
